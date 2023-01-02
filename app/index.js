9cf1b95665100f6482be346d22874f9faaa46898
= <kamblemardin@gmail.com>
2023-01-02 23:28:21 +0530

    finally connected database

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const port = 5432;
 
-const mysql = require("mysql");
 
 const dbConnection = mysql.createConnection({
   host: "localhost",
