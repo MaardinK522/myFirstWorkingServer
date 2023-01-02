9b74a5e15c784233056efce63a20160c1d5bcba3
MardinK522 <kamblemardin@gmail.com>
2023-01-02 23:24:26 +0530

    added database finally with connection

--- app/index.js
+++ app/index.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 5432
-var html_path = __dirname + '.\\htmls';
+const express = require("express");
+const app = express();
+const port = 5432;
 
-app.use(express.static(__dirname + '/htmls'));
+const mysql = require("mysql");
 
-app.get('/', (req, res) => {
-    res.sendFile("./htmls/index.html", {root:__dirname});
-})
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  port: 9898,
+  user: "root",
+  password: "root",
+  database: "privateserver",
+});
+
+app.use(express.static(__dirname + "/htmls"));
+
+app.get("/", (req, res) => {
+  res.sendFile("./htmls/index.html", { root: __dirname });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log("Project backend app listening on port " + port);
+  dbConnection.connect((err) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Successfully connected to Database');
+  });
+});
